860c3446e1bcba428d661b2039a4a3fbb60027cb
Monwifi <47664170+infrarik@users.noreply.github.com>
2026-06-13 15:07:27 +0200

    Update ButtonMQTTESP8266_ttp223.ino

--- ButtonMQTTESP8266_ttp223.ino
+++ ButtonMQTTESP8266_ttp223.ino
@@ -1,3 +1,9 @@
+//
+// Use TTP223 sensors as buttons
+//
+// Code updated with modified inputs and delays
+//
+
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPUpdateServer.h>
